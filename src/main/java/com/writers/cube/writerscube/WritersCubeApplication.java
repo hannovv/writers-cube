27d9912b9b6e0da2678c0cb61ef2ec096f979848
hannovv <hmnovack@gmail.com>
2023-07-19 15:41:34 -0400

    initial setup

---
+++ src/main/java/com/writers/cube/writerscube/WritersCubeApplication.java
@@ -0,0 +1,13 @@
+package com.writers.cube.writerscube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritersCubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritersCubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/writers/cube/writerscube/WritersCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writers.cube.writerscube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritersCubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
